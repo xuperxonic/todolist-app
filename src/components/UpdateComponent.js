1ea41a940d16b8aded9e324212a1bde185ab3818
xuperxonic <67827456+xuperxonic@users.noreply.github.com>
2022-08-17 19:13:18 +0900

    Update UpdateComponent.js

--- src/components/UpdateComponent.js
+++ src/components/UpdateComponent.js
@@ -8,12 +8,8 @@ class UpdateContent extends Component {
       subTitle: this.props.data.subTitle,
       subText: this.props.data.subText
     }
-    // 여기가 추가되었다. 정의한 함수를 state에도 바인딩을 시켜주는 것이 필요하다 
     this.inputFormHandler = this.inputFormHandler.bind(this);
   }
-	
-  // key 값에 [] 괄호를 씌우면 해당하는 key들 모두에 대응한다는 뜻으로, 특정 key를 따로 지정하지 않아도 된다. 
-  // 이렇게 변화된 값을 setState() 메서드로 상태값을 변경해주면 된다.
   inputFormHandler(e) {
     this.setState({
       [e.target.name]: e.target.value
@@ -35,7 +31,6 @@ class UpdateContent extends Component {
         }.bind(this)}>
           <p><input type="text" name="subTitle" placeholder="과목을 입력하세요"
             value={this.state.subTitle} onChange={this.inputFormHandler}
-               // onChange에 다음 handler들을 추가해준다. 
           ></input></p>
           <p><textarea name="subText" placeholder="내용을 입력하세요"
             value={this.state.subText} onChange={this.inputFormHandler}
